1827c14e06c23091fa4fecb169dde738343d972a
Natacha Romano <33462970+romanonatacha@users.noreply.github.com>
2018-06-02 13:21:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var bird;
+function setup () {
+    createCanvas(400, 600);
+    bird = new Bird();
+}
+
+function draw() {
+    background(0);
+    bird.update();
+    bird.show();
+}
+
+function keyPressed() {
+    if (key == ' ') {
+        bird.up();
+    }
+}
